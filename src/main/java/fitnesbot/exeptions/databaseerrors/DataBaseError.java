4e2ab405b79ec58436750ba83a8fd24440513f34
Fedor Grebnev <fedor.grebnev88@gmail.com>
2024-12-12 20:14:30 +0500

    DataBaseError base errors

---
+++ src/main/java/fitnesbot/exeptions/databaseerrors/DataBaseError.java
@@ -0,0 +1,14 @@
+package fitnesbot.exeptions.databaseerrors;
+
+public class DataBaseError extends Exception {
+        protected String message;
+
+        public DataBaseError(String message) {
+            this.message = message;
+        }
+
+        public String getErrorMessage() {
+            return message;
+        }
+
+}
